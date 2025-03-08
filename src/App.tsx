adc6e4f9a5f58dcd5fbcda51a6d4dd9ff717e8f1
Arkadiusz4 <arekmika4@gmail.com>
2025-03-08 22:19:58 +0100

    Format code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LandingPage from './pages/LandingPage';
 
 const App: React.FC = () => {
-  return <LandingPage/>;
+  return <LandingPage />;
 };
 
 export default App;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,27 +3,27 @@ import Navbar from '../components/LandingPage/Navbar/Navbar';
 import Hero from '../components/LandingPage/Hero/Hero';
 import WhyLionTrade from '../components/LandingPage/WhyLionTrade/WhyLionTrade';
 import './LandingPage.css';
-import LiveMarketInsights from "../components/LandingPage/LiveMarketInsights/LiveMarketInsights.tsx";
-import AIPredictions from "../components/LandingPage/AIPredictions/AIPredictions.tsx";
-import PricingPlans from "../components/LandingPage/PricingPlans/PricingPlans.tsx";
-import Footer from "../components/LandingPage/Footer/Footer.tsx";
-import JoinCTA from "../components/LandingPage/JoinCTA/JoinCTA.tsx";
+import LiveMarketInsights from '../components/LandingPage/LiveMarketInsights/LiveMarketInsights';
+import AIPredictions from '../components/LandingPage/AIPredictions/AIPredictions';
+import PricingPlans from '../components/LandingPage/PricingPlans/PricingPlans';
+import Footer from '../components/LandingPage/Footer/Footer';
+import JoinCTA from '../components/LandingPage/JoinCTA/JoinCTA';
 
 const LandingPage: React.FC = () => {
-    return (
-        <div className="landing-page">
-            <Navbar />
-            <main>
-                <Hero />
-                <WhyLionTrade />
-                <LiveMarketInsights />
-                <AIPredictions />
-                <PricingPlans />
-                <JoinCTA />
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="landing-page">
+      <Navbar />
+      <main>
+        <Hero />
+        <WhyLionTrade />
+        <LiveMarketInsights />
+        <AIPredictions />
+        <PricingPlans />
+        <JoinCTA />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
