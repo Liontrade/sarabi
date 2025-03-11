4cdd87463d88a95fb339425f13987945f8f33974
Przemyslaw Orlikowski <orprzemek@gmail.com>
2025-03-11 15:05:48 +0100

    Added react-icons, hamburger menu

--- src/components/LandingPage/Navbar/Navbar.tsx
+++ src/components/LandingPage/Navbar/Navbar.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import './Navbar.css';
-import logo from '../../../assets/logo_without_background.png';
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../../assets/logo_without_background.png";
+import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi"; 
 
 const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
         <img src={logo} alt="LionTrade Logo" className="navbar__logo-img" />
         <h1 className="navbar__brand">LionTrade</h1>
       </div>
-      <ul className="navbar__links">
+
+      {/* Hamburger Menu Button */}
+      <button className="navbar__toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </button>
+
+      {/* Links - Responsive Menu */}
+      <ul className={`navbar__links ${menuOpen ? "navbar__links--active" : ""}`}>
         <li>
           <a href="#why-liontrade">Why LionTrade?</a>
         </li>
@@ -27,9 +37,15 @@ const Navbar: React.FC = () => {
           <a href="#learn-grow">Learn &amp; Grow</a>
         </li>
       </ul>
+
+      {/* Login & Signup Buttons */}
       <div className="navbar__actions">
-      <Link to="/login" className="btn btn--login">Login</Link>
-          <Link to="/signup" className="btn btn--signup">Sign Up</Link>
+        <Link to="/login" className="btn btn--login">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn--signup">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
