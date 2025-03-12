9cb5bd4a05a81a8cec7ab46874eeaf0ba6c6ce43
Arkadiusz4 <arekmika4@gmail.com>
2025-03-12 18:14:24 +0100

    Add footer

---
+++ src/components/HomeDashboard/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Footer.css';
+
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__links">
+        <a href="#about">About</a>
+        <a href="#careers">Careers</a>
+        <a href="#security">Security</a>
+        <a href="#help-center">Help Center</a>
+        <a href="#privacy">Privacy</a>
+        <a href="#terms">Terms</a>
+      </div>
+
+      <div className="footer__social">
+        <FaTwitter className="footer__icon" />
+        <FaGithub className="footer__icon" />
+        <FaLinkedin className="footer__icon" />
+      </div>
+
+      <div className="footer__bottom">
+        &copy; 2025 LionTrade
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
