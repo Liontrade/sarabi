87924385780dd410de19909349286e05b70c01b1
Arkadiusz4 <arekmika4@gmail.com>
2025-03-14 11:03:06 +0100

    Add toast notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+// App.tsx
 import { JSX } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
-
 import LandingPage from './pages/LandingPage/LandingPage';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
@@ -9,17 +9,17 @@ import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import VerifyEmail from './pages/VerifyEmail/VerifyEmail';
 import Dashboard from './pages/Dashboard/Dashboard';
 import MarketPage from './pages/MarketPage/MarketPage';
-import Spinner from './components/Spinner/Spinner';
-
-import './styles/global.css';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
+import Spinner from './components/Spinner/Spinner';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/global.css';
 
 function App() {
     const { user, loading } = useAuth();
     console.log('App: user =', user, 'loading =', loading);
 
     const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-        console.log('ProtectedRoute: loading =', loading, ', user =', user);
         if (loading) {
             return (
                 <div
@@ -43,13 +43,24 @@ function App() {
 
     return (
         <Router>
+            {/* Dodaj ToastContainer */}
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/verify-email" element={<VerifyEmail />} />
-
                 <Route
                     path="/dashboard"
                     element={
@@ -74,7 +85,6 @@ function App() {
                         </ProtectedRoute>
                     }
                 />
-
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
