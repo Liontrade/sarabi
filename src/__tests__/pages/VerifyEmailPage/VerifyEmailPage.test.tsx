857ddda1c11f9b557723be44ae28dd0e2afc7644
Arkadiusz4 <arekmika4@gmail.com>
2025-04-25 14:52:39 +0200

    [fix] Correct tests v8

--- src/__tests__/pages/VerifyEmailPage/VerifyEmailPage.test.tsx
+++ src/__tests__/pages/VerifyEmailPage/VerifyEmailPage.test.tsx
@@ -1,5 +1,3 @@
-jest.useFakeTimers({ advanceTimers: true });
-
 import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
 import VerifyEmail from '../../../pages/VerifyEmailPage/VerifyEmailPage';
 import { sendEmailVerification, User } from 'firebase/auth';
@@ -69,9 +67,5 @@ describe('<VerifyEmail />', () => {
             expect(mockReload).toHaveBeenCalled();
             expect(mockNavigate).toHaveBeenCalledWith('/dashboard');
         });
-    });
-
-    afterAll(() => {
-        jest.useRealTimers();
     });
 });
